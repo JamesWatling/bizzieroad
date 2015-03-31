ca8ba5a73dec712d866662f60cf23b485dfcbae0
James Watling <watling.james@gmail.com>
2015-03-31 12:21:22 +0800

    ruby version

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,5 +1,5 @@
 FactoryGirl.create(:user, :confirmed)
 
-Provider.create(name: '2degrees', image: '/assets/two_degrees_logo.png')
-Provider.create(name: 'Vodafone', image: '/assets/vodafone_marker.png')
-Provider.create(name: 'Spark', image: '/assets/missing.png')
+Provider.create(name: '2degrees', logo: '/assets/two_degrees_logo.png')
+Provider.create(name: 'Vodafone', logo: '/assets/vodafone_marker.png')
+Provider.create(name: 'Spark', logo: '/assets/missing.png')
