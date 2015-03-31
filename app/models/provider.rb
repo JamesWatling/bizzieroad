1b74775b215d92b2222b40124ede322fd8a896e8
James Watling <watling.james@gmail.com>
2015-04-01 06:08:25 +0800

    importing csv and uploading icons to s3

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,6 +3,8 @@ class Provider < ActiveRecord::Base
   has_attached_file :default_icon, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :default_icon, :content_type => /\Aimage\/.*\Z/
 
+  attr_accessor :default_icon_file_name
+
   belongs_to :industry
   has_many :customers, through: :providers
 end
